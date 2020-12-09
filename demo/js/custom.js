83269821aed4208c0c2f5816e66997d616d1ee8e
Ajay gorana <54750602+AjayGorana@users.noreply.github.com>
2020-12-09 20:58:56 +0530

    Add files via upload

---
+++ demo/js/custom.js
@@ -0,0 +1,22 @@
+$(function () {
+	$("#jee-slider").owlCarousel({
+		items: 1,
+		autoplay: true,
+		smartSpeed: 700,
+		loop: true,
+    autoplayHoverPause: true,
+    
+	});
+});
+
+
+$(function () {
+	$("#neet-slider").owlCarousel({
+		items: 1,
+		autoplay: true,
+		smartSpeed: 700,
+		loop: true,
+    autoplayHoverPause: true,
+    
+	});
+});
